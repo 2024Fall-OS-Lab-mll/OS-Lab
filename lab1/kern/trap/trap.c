aa5a27ace5d948b36950e5b99d16fdfcefb32789
root <root@Y95>
2024-09-28 01:28:34 +0800

    lab1

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -105,13 +105,22 @@ void interrupt_handler(struct trapframe *tf) {
             // read-only." -- privileged spec1.9.1, 4.1.4, p59
             // In fact, Call sbi_set_timer will clear STIP, or you can clear it
             // directly.
-            // cprintf("Supervisor timer interrupt\n");
+            //cprintf("Supervisor timer interrupt\n");
              /* LAB1 EXERCISE2   YOUR CODE :  */
             /*(1)设置下次时钟中断- clock_set_next_event()
              *(2)计数器（ticks）加一
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
+            clock_set_next_event();
+            ticks++;
+            if(ticks%100==0){
+                print_ticks();
+                num++;
+                if(num==10){
+                    sbi_shutdown();
+                }
+            }
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
@@ -150,6 +159,9 @@ void exception_handler(struct trapframe *tf) {
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Exception type:Illegal instruction\n");
+            cprintf("Illegal instruction caught at 0x%x\n",tf->epc);
+            tf->epc=tf->epc+4;
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
@@ -158,6 +170,9 @@ void exception_handler(struct trapframe *tf) {
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Exception type:breakpoint\n");
+            cprintf("ebreak caught at 0x%x\n",tf->epc);
+            tf->epc=tf->epc+4;
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
@@ -198,4 +213,6 @@ static inline void trap_dispatch(struct trapframe *tf) {
  * the code in kern/trap/trapentry.S restores the old CPU state saved in the
  * trapframe and then uses the iret instruction to return from the exception.
  * */
+
+
 void trap(struct trapframe *tf) { trap_dispatch(tf); }
